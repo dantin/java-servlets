26a66664c34c371387dbff6295a1bbf00ef74558
David Ding <chengjie.ding@gmail.com>
2014-07-15 10:05:38 +0800

    add Http Codes

---
+++ HttpCodes/src/main/java/com/demo2do/servlets/ShowError.java
@@ -0,0 +1,21 @@
+package com.demo2do.servlets;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+public class ShowError extends HttpServlet {
+
+    // Method to handle GET method request.
+    public void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        // Set error code and reason.
+        response.sendError(407, "Need authentication!!!" );
+    }
+
+    // Method to handle POST method request.
+    public void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        doGet(request, response);
+    }
+}
