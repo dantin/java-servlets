b382632f25d7b7fbaa5c79041e4d71c7c80cd370
David Ding <chengjie.ding@gmail.com>
2014-07-23 15:16:39 +0800

    add date handling

---
+++ HandlingDate/src/main/java/com/demo2do/servlets/FormatDate.java
@@ -0,0 +1,31 @@
+package com.demo2do.servlets;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+public class FormatDate extends HttpServlet {
+
+    public void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        // Set response content type
+        response.setContentType("text/html");
+
+        PrintWriter out = response.getWriter();
+        String title = "Display Current Date & Time";
+        Date dNow = new Date( );
+        SimpleDateFormat ft = new SimpleDateFormat ("E yyyy.MM.dd 'at' hh:mm:ss a zzz");
+        String docType = "<!doctype html public \"-//w3c//dtd html 4.0 transitional//en\">\n";
+        out.println(docType +
+                "<html>\n" +
+                "<head><title>" + title + "</title></head>\n" +
+                "<body bgcolor=\"#f0f0f0\">\n" +
+                "<h1 align=\"center\">" + title + "</h1>\n" +
+                "<h2 align=\"center\">" + ft.format(dNow) + "</h2>\n" +
+                "</body></html>");
+    }
+}
