d61490d4c64bd5eef2686df236bef587f19460f0
David Ding <chengjie.ding@gmail.com>
2014-08-21 07:47:09 +0800

    restructure sample source

--- DatabaseAccess/src/main/resources/data.sql
+++
@@ -1,4 +0,0 @@
-INSERT INTO Employees VALUES (100, 18, 'Zara', 'Ali');
-INSERT INTO Employees VALUES (101, 25, 'Mahnaz', 'Fatma');
-INSERT INTO Employees VALUES (102, 30, 'Zaid', 'Khan');
-INSERT INTO Employees VALUES (103, 28, 'Sumit', 'Mittal')
--- DatabaseAccess/src/main/resources/schema.sql
+++
@@ -1,7 +0,0 @@
-use TEST;
-create table Employees (
-     id int not null,
-     age int not null,
-     first varchar (255),
-     last varchar (255)
-);
