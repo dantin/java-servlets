dcf54cf32c564be3a6d24975509a766f7634d733
David Ding <chengjie.ding@gmail.com>
2014-08-25 07:43:38 +0800

    Writing Filters

---
+++ src/main/java/com/demo2do/servlets/writingfilters/LogFilter.java
@@ -0,0 +1,32 @@
+package com.demo2do.servlets.writingfilters;
+
+import javax.servlet.*;
+import java.io.IOException;
+import java.util.Date;
+
+public class LogFilter implements Filter {
+
+    public void init(FilterConfig config) throws ServletException {
+        // Get init parameter
+        String testParam = config.getInitParameter("test-param");
+
+        //Print the init parameter
+        System.out.println("Test Param: " + testParam);
+    }
+
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
+
+        // Get the IP address of client machine.
+        String ipAddress = request.getRemoteAddr();
+
+        // Log the IP address and current timestamp.
+        System.out.println("IP " + ipAddress + ", Time " + new Date().toString());
+
+        // Pass request back down the filter chain
+        chain.doFilter(request, response);
+    }
+
+    public void destroy() {
+        /* Called before the Filter instance is removed from service by the web container*/
+    }
+}
